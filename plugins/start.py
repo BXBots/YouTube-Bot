a832578f458d07cd6c3e1712bffa4c8522847dfc
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-14 13:46:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🤖 Update Channel", url="https://t.me/BX_Botz"),
+        [InlineKeyboardButton("Channel ⭕️", url="https://t.me/EKBOTZ_UPDATE"),
         [InlineKeyboardButton(
-            "👥Support Group", url="https://t.me/BxSupport")]])
-    welcomed = f"Hai <b>{message.from_user.mention}</b>\n\n`Iam simple YouTube downloader bot. I Will Convert Youtube Link to Video/File & Mp3`.\n\n`For more details Press` /help.\n\n👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : [ʙx ʙᴏᴛᴢ](https://t.me/BX_Botz)"
+            "Support 🔰", url="https://t.me/ekbotz_support")]])
+    welcomed = f"Hai <b>{message.from_user.mention}</b>\n\n`Iam simple YouTube downloader bot. I Will Convert Youtube Link to Video/File & Mp3.` \n\nFor more details Press /help. \n\n👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : [ʙx ʙᴏᴛᴢ](https://t.me/BX_Botz)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
