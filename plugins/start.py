bda1946a3249ff631f0de7506d7044e3969f0f8e
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-14 13:33:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,8 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("🤖 Update Channel", url="https://t.me/BX_Botz"),
-        [InlineKeyboardButton("👥Support Group", url="https://t.me/BxSupport")]])
+        [InlineKeyboardButton(
+            "👥Support Group", url="https://t.me/BxSupport")]])
     welcomed = f"Hai <b>{message.from_user.mention}</b>\n\n`Iam simple YouTube downloader bot. I Will Convert Youtube Link to Video/File & Mp3`.\n\n`For more details Press` /help.\n\n👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : [ʙx ʙᴏᴛᴢ](https://t.me/BX_Botz)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
